e1a835ec63225361814d8586fe08552e2da2fe61
Silvana <silvanabelen.d@gmail.com>
2021-08-20 13:45:33 -0400

    septimo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,21 @@ $(function(){
  $('[data-toggle="popover"]').popover()
  
  $('[data-toggle="tooltip"]').tooltip()
+ $(document).ready(function(){
 
+	$('.button-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.button-up').slideDown(300);
+		} else {
+			$('.button-up').slideUp(300);
+		}
+	});
+
+});
 });
